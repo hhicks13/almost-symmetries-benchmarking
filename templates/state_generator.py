9f45f961d5997c9a51a1d3b7e4fd72f91ade9602
Harrison Hicks <hhicks13@vols.utk.edu>
2020-11-16 16:11:54 -0500

    coroutine finite-state machine implemented, see charming python

--- templates/state_generator.py
+++ templates/state_generator.py
@@ -7,54 +7,66 @@ def math_gen(n): #iterative function is generator
     while 1:
         yield n
         n = abs(sin(n))*31
-        
+def step_one(n):
+    global cargo
+    int colcount
+    for row in row:
+        for column in columns:
+            if M[row,column] == 1:
+                Colcover = 1
+
+
+
 # jump targets are independent of state
 def jump_to(val):
-    if 0<= val < 10: return 'ONES'
-    elif 10 <= val <20: return 'TENS'
-    elif 20 <= val < 30: return 'TWENTIES'
-    else: return 'OTHER INTERVAL'
+    if 0 <= val < 10: return f"ONES"
+    elif 10 <= val <20: return f"TENS"
+    elif 20 <= val < 30: return f"TWENTIES"
+    else: return f"OTHER INTERVAL"
 
 # get targets are state-sensitive and map from iter to new state
 def get_ones(iter):
     global cargo
     while 1:
-        print("\nONES State:    ",end="") #trailing comma continues evaluation
+        print("\nONES     State:  ",end="") #trailing comma continues evaluation
         while jump_to(cargo)=='ONES':
-            print("{}".format(cargo),end="") #trailing comma continues evaluation
-            cargo = iter.next()
-            yield (jump_to(cargo),cargo)
+            print(f'@{cargo:.17f}    ',end="") #trailing comma continues evaluation
+            cargo = next(iter)
+        yield (jump_to(cargo),cargo)
 # get targets are state-sensitive and map from iter to new state
 def get_tens(iter):
     global cargo
     while 1:
-        print("\nTENS State:    ",end="") #trailing comma continues evaluation
+        print("\nTENS     State:  ",end="") #trailing comma continues evaluation
         while jump_to(cargo)=='TENS':
-            print("{}".format(cargo),end="") #trailing comma continues evaluation
-            cargo = iter.next()
-            yield (jump_to(cargo),cargo)
+            print(f'#{cargo:.17f}    ',end="") #trailing comma continues evaluation
+            cargo = next(iter)
+        yield (jump_to(cargo),cargo)
 # get targets are state-sensitive and map from iter to new state
 def get_twenties(iter):
     global cargo
     while 1:
-        print("\nTWENTIES State:    ",end="") #trailing comma continues evaluation
+        print("\nTWENTIES State:  ",end="") #trailing comma continues evaluation
         while jump_to(cargo)=='TWENTIES':
-            print("{}".format(cargo),end="") #trailing comma continues evaluation
-            cargo = iter.next()
-            yield (jump_to(cargo),cargo)
+            print(f"*{cargo:.17f}    ",end="") #trailing comma continues evaluation
+            cargo = next(iter)
+        yield (jump_to(cargo),cargo)
 # halting condition
 def exit(iter):
-    jump = raw_input('\n\n[co-routine for jump?] ').upper()
-    print ("...Jumping into middle of ",jump,end="")
-    yield (jump, iter.next())
-    print ("\nExiting from exit()...",end="")
+    jump = input('\n\n[manual co-routine for jump?] ')
+    print ("...Jumping into middle of ",jump.upper())
+    yield (f'{jump.upper()}', next(iter))
+    print ("\nExiting from exit()...")
     sys.exit()
 
 def scheduler(gendct, start):
     global cargo
     coroutine = start
     while 1:
-        (coroutine, cargo) = gendct[coroutine].next()
+        (coroutine, cargo) = next(gendct[coroutine])
+        if coroutine == '':
+            print("special exit")
+            sys.exit()
 
 # Scheduler here implements the state machine and is generic
 # requires a dictionary of generator objects that have been instantiated
@@ -64,10 +76,13 @@ def scheduler(gendct, start):
 # the iterator math_gen is designed so that the iterator function does not need to manage past states, or pass values backwards and issues an indefinite stream.
 # this convention is crucial in isolating the state transition function. 
 if __name__ == "__main__":
+    #initial value
     num_stream = math_gen(1)
-    cargo = num_stream.next()
+    cargo = next(num_stream) # computes math_gen again 
+    # predefined states, each with transition function operating on shared stream.
+    # in the munkres algorithm they are operating on 
     gendct = {'ONES'        : get_ones(num_stream),
               'TENS'        : get_tens(num_stream),
               'TWENTIES'    : get_twenties(num_stream),
-              'OUT_OF_RANGE': exit(num_stream)         }
+              'OTHER INTERVAL': exit(num_stream)         }
     scheduler(gendct, jump_to(cargo))
